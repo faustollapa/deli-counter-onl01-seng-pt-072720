4625abc9c6434248fb97e8d776173b776aec9c02
Fausto Llapa <faustollapa850@gmail.com>
2020-08-03 15:07:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,42 @@
 # Write your code here.
-function takeANumber(katzDeliLine, name){
-    katzDeliLine.push(name);
-    return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-}
+var katzDeli = [];
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return "Currently serving " + katzDeliLine.shift() + ".";
-  } else {
-      return "There is nobody waiting to be served!";
-  }
-}
-
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    var output = "";
-    for(var i = 0; i < katzDeliLine.length; i++) {
-        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
-      }
-    output = output.slice(0, -2);
-    return `The line is currently: ${output}`;
-  } else {
+function currentLine(line){
+  if(!line.length) {
     return "The line is currently empty.";
   }
+  var lineNamesandNumbers = [];
+  
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
+
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli)
