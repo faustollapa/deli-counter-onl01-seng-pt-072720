c11a9b2c70869b2b05bd78eca49c609e3e4c3443
Fausto Llapa <faustollapa850@gmail.com>
2020-08-03 15:11:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,48 +1,30 @@
 # Write your code here.
-var katzDeli = []; 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-}
-function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
-var line = [];
+var katzDeliLine = [];
 
-
-
-
-
-function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-//WORKS:::::
-
-// function currentLine(katzDeli) {
-//
-//   for (let i = 0; i < katzDeli.length; i++) {
-//     line.push(` `+[i+1]+`. `  + katzDeli[i])
-//   }
-//   return(`The line is currently:` + line);
-// }
-
-
-  
-  
+function currentLine(NewLine) {
+    var line = []
+    if (NewLine.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < NewLine.length; i++) {
+        line += (i + 1) + ". " + NewLine[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
+    //
+}
+function nowServing(x) {
+  if (x.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = x[0];
+    x.splice(0, 1);
+    return "Currently serving " + name + ".";
+  }
+}
